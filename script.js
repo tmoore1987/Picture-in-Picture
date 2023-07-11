84318658d8c27307da59f4949d8c041e0f26f89e
Taylor Moore <122215363+tmoore1987@users.noreply.github.com>
2023-07-11 17:29:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream () {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+
+    } catch (error) {
+        // Catch error here
+        console.log('Error here', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    //Disable the button
+    button.disabled = true;
+    //Start picture in picture
+    await videoElement.requestPictureInPicture();
+    //reset button
+    button.disabled = false;
+});
+
+//On load
+selectMediaStream();
